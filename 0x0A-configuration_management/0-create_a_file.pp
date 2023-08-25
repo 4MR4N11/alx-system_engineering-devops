b60a90fc90509bab300e74cf59a037c04c6ed5e0
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-08-25 16:01:26 +0100

    Mandatory part

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 # creating a file in /tmp
 file { '/tmp/school':
-  ensure => 'present',
-  content => 'I love Puppet',
   mode => '0744',
   owner => 'www-data',
   group => 'www-data',
+  content => 'I love Puppet',
 }
