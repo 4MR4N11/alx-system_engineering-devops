726d49251468078ecbd4eb2b42ada1137006dbeb
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-10-10 14:31:39 +0100

    Mandatory part

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,6 @@ import requests
 
 def top_ten(subreddit):
     """Prints the titles of the top 10 hot posts for a given subreddit"""
-
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'Test'}
     response = requests.get(url, headers=headers).json()
