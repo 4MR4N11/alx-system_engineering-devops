d894d3ab161bcbf4512c763eb30b2bc4fafa18c9
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-10-10 14:32:52 +0100

    Mandatory part

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,9 +8,11 @@ def top_ten(subreddit):
     """Prints the titles of the top 10 hot posts for a given subreddit"""
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'Test'}
-    response = requests.get(url, headers=headers).json()
-    try:
-        for i in range(10):
-            print(response['data']['children'][i]['data']['title'])
-    except Exception:
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json().get('data')
+        children = data.get('children')
+        for child in children[:10]:
+            print(child.get('data').get('title'))
+    else:
         print(None)
