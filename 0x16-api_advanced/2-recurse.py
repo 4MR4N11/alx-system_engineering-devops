1c919731d61fe666c7b6cd2bcd580656efe121d9
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-11-07 20:19:33 +0100

    Mandatory part

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
 """ module 2-recurse.py """
 import requests
-after = None
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=""):
     """Returns a list containing the titles of all hot
     articles for a given subreddit
     """
-
-    global after
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Test"}
-    param = {"after": after}
-    response = requests.get(url, headers=headers, params=param,
+    headers = {"User-Agent": "TEST"}
+    params = {"after": after}
+    response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 200:
-        for i in range(len(response.json().get("data").get("children"))):
-            hot_list.append(response.json().get("data")
-                            .get("children")[i].get("data").get("title"))
-        after = response.json().get("data").get("after")
-        if after is not None:
-            recurse(subreddit, hot_list)
-        return hot_list
+        data = response.json().get("data")
+        after = data.get("after")
+        children = data.get("children")
+        for child in children:
+            hot_list.append(child.get("data").get("title"))
+        if after is None:
+            return hot_list
+        return recurse(subreddit, hot_list, after)
     else:
         return None
