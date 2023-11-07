33093c52738aea277bf33f278be2c443f6111e25
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-11-07 15:24:04 +0100

    Mandatory part

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,12 +4,12 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-	"""Returns the number of subscribers for a given subreddit"""
+    """Returns the number of subscribers for a given subreddit"""
 
-	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	headers = {"User-Agent": "Test"}
-	response = requests.get(url, headers=headers, allow_redirects=False)
-	if response.status_code == 200:
-		return response.json().get("data").get("subscribers")
-	else:
-		return 0
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Test"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
+    else:
+        return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,13 +4,14 @@ import requests
 
 
 def top_ten(subreddit):
-	"""Prints the titles of the top 10 hot posts for a given subreddit"""
+    """Prints the titles of the top 10 hot posts for a given subreddit"""
 
-	url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-	headers = {"User-Agent": "Test"}
-	response = requests.get(url, headers=headers, allow_redirects=False)
-	if response.status_code == 200:
-		for i in range(10):
-			print(response.json().get("data").get("children")[i].get("data").get("title"))
-	else:
-		print(None)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Test"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        for i in range(10):
+            print(response.json().get("data").get("children")[i]
+                  .get("data").get("title"))
+    else:
+        print(None)
