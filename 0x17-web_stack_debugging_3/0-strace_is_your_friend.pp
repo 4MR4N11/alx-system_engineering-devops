023b3c9a3c44214d01020e4cc943c02b4255d5fc
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-11-08 14:21:32 +0100

    Mandatory part

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix wordpress settings
 
-exec { 'wordpressFix'
+exec { 'wordpressFix':
   command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
   path    => '/bin:/usr/bin:/usr/local/bin'
 }
